a0865832aa5b984c244d46dc1239b1f613108603
millecodex <39792005+millecodex@users.noreply.github.com>
2020-01-22 13:45:12 +1300

    Add files via upload

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,11 +3,16 @@ pragma solidity ^0.5.0;
 contract SimpleStorage {
     //this variable will be stored on the blockchain (storage)
     string public data;
-    
+
     //type--location (temp)--name
     function set(string memory _data) public {
     //to access the storage variable (above) inside the function
     //often will see the underscore because 'data' is used twice
         data = _data;
+    }
+    //view is a keyword similar to pure that reads 
+    //the storage of a contract (readonly)
+    function get() view public returns(string memory){
+        return data;
     }
 }
