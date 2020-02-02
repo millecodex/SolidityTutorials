9587ed669e39eb5903546ae618dbed1fb605ef15
millecodex <39792005+millecodex@users.noreply.github.com>
2020-02-02 19:29:11 +1300

    Add files via upload

--- zombiefactory.sol
+++ zombiefactory.sol
@@ -1,8 +1,13 @@
 pragma solidity >=0.5.0 <0.6.0;
 
 import "./ownable.sol"
+import "./safemath.sol"
 
 contract ZombieFactory is Ownable{
+
+    //declare that safemath is being used
+    //'using' means safemath is a 'library'
+    using SafeMath for uint256;
 
     // declare our event here
     event NewZombie(uint zombieId, string name, uint dna);
