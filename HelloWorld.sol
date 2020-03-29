47435a54ae7b5939c647ac1be978fa886f3acc76
millecodex <39792005+millecodex@users.noreply.github.com>
2020-03-30 09:37:01 +1300

    Update HelloWorld.sol

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -1,11 +1,39 @@
-pragma solidity ^0.5.0;
+pragma solidity ^0.6.4;
 
-contract HelloWorld{
+contract HelloWorld {
+    uint256 jeff = 1;
+    
     //'public' allows the hello() function to be called outside the smart contract
     //'returns' needs 's' to specify the return type
     //'memory' is the location
     //'pure' means no modification to the blockchain
-    function hello() pure public returns(string memory){
-        return 'Helllo World';
+    
+    //does NOT consume gas
+    function hello() public pure returns(string memory){
+        return 'hello there amigo';    
     }
+    
+    //does consume gas
+    function getJeff() public returns(uint){
+        jeff++;
+        return jeff;
+    }
+    
+    //when this function is called, ether is sent along with it (value)
+    function donate() external payable returns(uint){
+    }
+    
+    function getBalance() external view returns(uint){
+        //output contract balance
+        return address(this).balance;
+    }
+    
+    //the caller's value will be sent to _recipient
+    function send(address payable _recipient) public payable{
+        _recipient.transfer(msg.value);
+    }
+    
+    //fallback function: default if no other function matches the caller
+    fallback() external payable {}
+    
 }
